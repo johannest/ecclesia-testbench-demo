16bf7db5bf596e2228ed55557bba8621bd0e6969
Johannes Tuikkala <johannes@vaadin.com>
2024-07-10 11:59:14 +0300

    next try

--- src/test/java/views/BasicUiTestIT.java
+++ src/test/java/views/BasicUiTestIT.java
@@ -8,9 +8,11 @@ import com.vaadin.testbench.Parameters;
 import com.vaadin.testbench.annotations.RunLocally;
 import com.vaadin.testbench.parallel.Browser;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.openqa.selenium.Dimension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Execution(ExecutionMode.SAME_THREAD)
@@ -18,14 +20,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 @RunLocally(Browser.CHROME)
 public class BasicUiTestIT extends BrowserTestBase {
 
+	@BeforeAll
+	protected static void setupClass() {
+		//if (Boolean.getBoolean("ghActions")) {
+		System.out.println("---- ghActions ----");
+		Parameters.setHeadless(true);
+		Parameters.setChromeOptions("--no-sandbox --disable-extensions "
+									+ "--disable-gpu --disable-dev-shm-usage");
+		//}
+	}
+
 	@BeforeEach
 	protected void setUp() {
-		if (Boolean.getBoolean("ghActions")) {
-			System.out.println("---- ghActions ----");
-			Parameters.setHeadless(true);
-			Parameters.setChromeOptions("--no-sandbox --disable-extensions "
-										+ "--disable-gpu --disable-dev-shm-usage");
-		}
+		getDriver().manage().window().setSize(new Dimension(1920, 1080));
 		getDriver().get("http://localhost:8080");
 		getCommandExecutor().waitForVaadin();
 	}
